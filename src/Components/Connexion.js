3e4efc401c48079f97f1acdbaae4f694ebd31144
vcaquant <vcaquant@student.42.fr>
2017-05-30 14:16:19 +0200

    retest

---
+++ src/Components/Connexion.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Connexion extends React.Component {
+
+    goToChat = event => {
+        event.preventDefault();
+        const pseudo = this.pseudoInput.value;
+        this.context.router.transitionTo(`/pseudo/${pseudo}`);
+    };
+
+    render() {
+        return (
+            <div className="connexionBox" onSubmit={e => this.goToChat(e)} >
+                <form className="connexion">
+                    <div className="center">
+                        <img src="logo.png" alt="logo" height="200" width="200"/>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Pseudo"
+                        required
+                        ref={input => {this.pseudoInput = input}} />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        required />
+                    <button type="submit">OK</button>
+                </form>
+            </div>
+        )
+    }
+    
+    static contextTypes = {
+        router: React.PropTypes.object
+    }
+}
+
+export default Connexion;
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+// Components
+import Connexion from './Components/Connexion';
+import App from './Components/App';
+import NotFound from './Components/NotFound';
+// Rooter
+import { BrowserRouter, Match, Miss } from 'react-router';
+// CSS
+import './index.css';
+
+const Root = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={Connexion} />
+                <Match pattern="/pseudo/:pseudo" component={App} />
+                <Miss component={NotFound} />
+            </div>
+        </BrowserRouter>
+    )
+}
+
+render(
+    <Root />,
+    document.getElementById('root')
+);
