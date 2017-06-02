5370b3f4434b51a6956703b412c8dcbc8fa5be97
vcaquant <vcaquant@student.42.fr>
2017-06-02 23:53:26 +0200

    add markdown to message

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,20 +1,24 @@
 import React from 'react';
+import marked from 'marked';
 
 class Message extends React.Component {
+
+    renderText = (text) => {
+        const renderText = marked(text, {sanitize: true});
+        return { __html: renderText };
+    };
 
     preRender = isUser => {
         if (isUser) {
             return (
-                <p className="user-message">
-                    {this.props.details.message}
-                </p>
+                <p className="user-message" dangerouslySetInnerHTML={this.renderText(this.props.details.message)} />
             )
         }
         else {
             return (
                 <p className="not-user-message">
-                    <strong>{this.props.details.pseudo}</strong>: 
-                        {this.props.details.message}
+                    <strong>{this.props.details.pseudo}</strong>:
+                        <p dangerouslySetInnerHTML={this.renderText(this.props.details.message)} />
                 </p>
             )
         }
